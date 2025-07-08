0f410f7e942f9aae66885a113ce552e07de663df
sinmetal <sinmetal@sinmetalcraft.jp>
2025-07-08 14:24:17 +0900

    CloudTrace自身に送るspanである `google.devtools.cloudtrace.v2.TraceService/BatchWriteSpans` はノイジーなので、フィルタリングした

--- go.mod
+++ go.mod
@@ -7,9 +7,11 @@ require (
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace v1.29.0
 	github.com/google/uuid v1.6.0
 	github.com/sinmetalcraft/gcpbox v1.25.2
+	go.opencensus.io v0.24.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0
 	go.opentelemetry.io/otel v1.36.0
 	go.opentelemetry.io/otel/sdk v1.36.0
+	go.opentelemetry.io/otel/trace v1.36.0
 	google.golang.org/api v0.237.0
 )
 
@@ -39,13 +41,11 @@ require (
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
 	github.com/spiffe/go-spiffe/v2 v2.5.0 // indirect
 	github.com/zeebo/errs v1.4.0 // indirect
-	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/detectors/gcp v1.36.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.61.0 // indirect
 	go.opentelemetry.io/otel/metric v1.36.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.36.0 // indirect
-	go.opentelemetry.io/otel/trace v1.36.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
--- internal/trace/trace.go
+++ internal/trace/trace.go
@@ -8,7 +8,34 @@ import (
 	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
 	"go.opentelemetry.io/otel"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 )
+
+// batchWriteSpansName is the name of the span to be filtered.
+const batchWriteSpansName = "google.devtools.cloudtrace.v2.TraceService/BatchWriteSpans"
+
+// filteringSampler is a sampler that filters spans based on their name.
+type filteringSampler struct {
+	delegate sdktrace.Sampler
+}
+
+// ShouldSample implements sdktrace.Sampler.
+func (s filteringSampler) ShouldSample(p sdktrace.SamplingParameters) sdktrace.SamplingResult {
+	// if the span name is batchWriteSpansName, drop it.
+	if p.Name == batchWriteSpansName {
+		return sdktrace.SamplingResult{
+			Decision:   sdktrace.Drop,
+			Tracestate: trace.SpanContextFromContext(p.ParentContext).TraceState(),
+		}
+	}
+	// For other spans, use the delegate sampler.
+	return s.delegate.ShouldSample(p)
+}
+
+// Description implements sdktrace.Sampler.
+func (s filteringSampler) Description() string {
+	return fmt.Sprintf("filteringSampler{%s}", s.delegate.Description())
+}
 
 func InitTracer(projectID string) (func(), error) {
 	exporter, err := texporter.New(texporter.WithProjectID(projectID))
@@ -16,7 +43,17 @@ func InitTracer(projectID string) (func(), error) {
 		return nil, fmt.Errorf("failed to create exporter: %w", err)
 	}
 
-	tp := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter))
+	sampler := filteringSampler{
+		// Use ParentBased sampler with AlwaysSample as the root sampler.
+		// This means that if there is a parent span, the child span will inherit the sampling decision.
+		// If there is no parent span, the child span will always be sampled.
+		delegate: sdktrace.ParentBased(sdktrace.AlwaysSample()),
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithSampler(sampler),
+	)
 	otel.SetTracerProvider(tp)
 
 	return func() {
